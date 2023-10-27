6ba75f8620f48a28256a113608fc01d76c334fbf
Jhonatan Henkel <jhowrf@gmail.com>
2023-10-26 22:16:09 -0300

    Desenvolvido controller base

---
+++ app/Http/Response/ResponseError.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Response;
+
+use Illuminate\Http\JsonResponse;
+
+class ResponseError
+{
+    public static function responseError(string $error, int $statusCode): JsonResponse
+    {
+        $message = json_decode($error, true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            $message = $error;
+        }
+        return response()->json(['message' => $message], $statusCode);
+    }
+}
