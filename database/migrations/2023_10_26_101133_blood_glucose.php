3890b662d53e59a2174fdbab507000c9ca839508
Jhonatan Henkel <jhowrf@gmail.com>
2023-10-26 21:57:59 -0300

    Modelando tabelas

---
+++ database/migrations/2023_10_26_101133_blood_glucose.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blood_glucose', function (Blueprint $table) {
+            $table->id();
+            $table->string('blood_glucose', 3)->nullable(false);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blood_glucose');
+    }
+};
