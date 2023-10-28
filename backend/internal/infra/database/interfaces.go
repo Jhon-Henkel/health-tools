0d796a98d952f9302f9a1b4c70bd9837cec710cb
Jhonatan Henkel <jhowrf@gmail.com>
2023-10-27 22:00:10 -0300

    Desenvolvido interfaces

---
+++ backend/internal/infra/database/interfaces.go
@@ -0,0 +1,15 @@
+package database
+
+import "github.com/Jhon-Henkel/health-tools/tree/main/backend/internal/entity"
+
+type BloodPressureInterface interface {
+	Create(bloodPressure *entity.BloodPressure) (*entity.BloodPressure, error)
+	FindAll(page, limit int, sort string) ([]*entity.BloodPressure, error)
+	Delete(id string) error
+}
+
+type BloodGlucoseInterface interface {
+	Create(bloodGlucose *entity.BloodGlucose) (*entity.BloodGlucose, error)
+	FindAll(page, limit int, sort string) ([]*entity.BloodGlucose, error)
+	Delete(id string) error
+}
