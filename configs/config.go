3e0baf0f10397a2e18f44da68e72a3206e5bf8eb
Jhonatan Henkel <jhowrf@gmail.com>
2023-10-28 16:02:57 -0300

    Voltando ajustes

--- configs/config.go
+++ configs/config.go
@@ -1,9 +1,6 @@
 package configs
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/go-chi/jwtauth"
 	"github.com/spf13/viper"
 )
@@ -29,21 +26,15 @@ func LoadConfig(path string) (*config, error) {
 	viper.SetConfigFile(".env")
 	viper.AutomaticEnv()
 	err := viper.ReadInConfig()
-	if err != nil {
-		fmt.Println("No config file loaded - using env variables")
-		cfg.DbDriver = os.Getenv("DB_DRIVER")
-		cfg.DbHost = os.Getenv("DB_HOST")
-		cfg.DbPort = os.Getenv("DB_PORT")
-		cfg.DbUser = os.Getenv("DB_USER")
-		cfg.DbPassword = os.Getenv("DB_PASSWORD")
-		cfg.DbName = os.Getenv("DB_NAME")
-		cfg.WebServerPort = os.Getenv("WEB_SERVER_PORT")
-	} else {
-		err = viper.Unmarshal(&cfg)
-		if err != nil {
-			panic(err)
-		}
-	}
+	checkError(err)
+	err = viper.Unmarshal(&cfg)
+	checkError(err)
 	cfg.TokenAuthKey = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
 	return cfg, err
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
