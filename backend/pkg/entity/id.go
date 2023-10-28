92b5e594946d701f6dcb7492e1bfcada850d0352
Jhonatan Henkel <jhowrf@gmail.com>
2023-10-27 21:36:41 -0300

    Desenvolvido package para ID

---
+++ backend/pkg/entity/id.go
@@ -0,0 +1,14 @@
+package entity
+
+import "github.com/google/uuid"
+
+type ID = uuid.UUID
+
+func NewID() ID {
+	return ID(uuid.New())
+}
+
+func ParseID(inputId string) (ID, error) {
+	id, err := uuid.Parse(inputId)
+	return ID(id), err
+}
