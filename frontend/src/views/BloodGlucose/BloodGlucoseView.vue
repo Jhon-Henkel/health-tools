1f30aeeed11e8fe6251ccc13bc766be9ed5db0b4
Jhonatan Henkel <jhowrf@gmail.com>
2023-10-28 21:56:58 -0300

    Desenvolvendo listagem base de glicemia

---
+++ frontend/src/views/BloodGlucose/BloodGlucoseView.vue
@@ -0,0 +1,84 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>
+          Glicemia
+        </ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-content color="light">
+        <ion-row>
+          <ion-col>
+            <ion-datetime-button datetime="datetime" />
+            <ion-modal :keep-contents-mounted="true">
+              <ion-datetime presentation="month-year" id="datetime" />
+            </ion-modal>
+            <ion-list :inset="true">
+              <ion-item>
+                <ion-label>Glicemia</ion-label>
+                <ion-label>Data</ion-label>
+                <ion-label>Hora</ion-label>
+              </ion-item>
+              <ion-item>
+                <ion-label>92</ion-label>
+                <ion-label>25/10</ion-label>
+                <ion-label>15:30</ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-col>
+        </ion-row>
+        <ion-button class="ion-margin-horizontal ion-margin-top" expand="block" href="/glicemia/registrar">
+          Inserir Novo Registro
+        </ion-button>
+      </ion-content>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+  import {
+    IonContent,
+    IonHeader,
+    IonItem,
+    IonToolbar,
+    IonTitle,
+    IonPage,
+    IonList,
+    IonButton,
+    IonRow,
+    IonCol,
+    IonLabel,
+    IonDatetimeButton,
+    IonModal,
+    IonDatetime
+  } from '@ionic/vue';
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    components: {
+      IonContent,
+      IonHeader,
+      IonItem,
+      IonToolbar,
+      IonTitle,
+      IonPage,
+      IonList,
+      IonButton,
+      IonRow,
+      IonCol,
+      IonLabel,
+      IonDatetimeButton,
+      IonModal,
+      IonDatetime
+    }
+  });
+</script>
+
+<style scoped>
+
+  ion-datetime {
+    border-radius: 16px;
+  }
+</style>
